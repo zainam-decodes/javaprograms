9a6bb3fc79843b4f59559acac9577378c8deb9a4
Zainab Jahan Umaima <thezainabjahan14@gmail.com>
2025-02-13 11:14:04 +0530

    Add files via upload
    BASIC PROGRAM FOR INPUTTING STRING

---
+++ details.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class details{
+        public static void main(String[] args){
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the name:");
+        String name=sc.next();
+        System.out.println("Hi"+" "+name);
+
+        }
+}
