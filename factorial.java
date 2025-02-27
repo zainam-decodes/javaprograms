48bb110171d4dd7f90ca9a97e8a9663967b41b26
Zainab Jahan Umaima <thezainabjahan14@gmail.com>
2025-02-27 11:08:05 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,10 @@
+class fact{
+    public static void main(String[] ar){
+         int a=4,f=1;
+          for(int i=1;i<=a;i++){
+              f=f*i;
+           }
+              System.out.println("factorial of "+a+" is "+f);
+      }
+}
+            
