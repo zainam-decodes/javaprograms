56804b602b358970c9cd202b6b00e075a58043d2
Zainab Jahan Umaima <thezainabjahan14@gmail.com>
2025-04-03 10:23:25 +0530

    Add files via upload

---
+++ stringbuild.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class StringBuild{
+       public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter the String");
+            String s1=sc.next();
+            s1.concat("hey");
+            System.out.println("String after concatenation is  "+s1);
+            StringBuilder sb=new StringBuilder("hello");
+            sb.append("hi");
+            System.out.println("string after appending is "+sb);
+          }
+}
