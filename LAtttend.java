371fa5aff63c7061b18bce9b853d40b3c388970d
Zainab Jahan Umaima <thezainabjahan14@gmail.com>
2025-05-08 19:43:51 +0530

    Update LAtttend.java

--- LAtttend.java
+++ LAtttend.java
@@ -1,40 +1,43 @@
 import java.io.*;
-   class LAttend extends Exception {
-   Lattend(String str) {
-      super(str);
-  }
+class LAttend extends Exception 
+{
+       LAttend(String str) 
+      {
+            super(str);
+       }
            public static void main(String[] args) throws IOException {
-              BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-              System.out.println("Enter the record submission status as (true/false)");
-              String ans=br.readLine();
-              boolean recordSubmitted = ans.equalsIgnoreCase("false);
+                  BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+                  System.out.println("Enter the record submission status as (true/false)");
+                  String ans=br.readLine();
+                  boolean recordSubmitted = ans.equalsIgnoreCase("false");
                 try {
-                   System.out.println("Enter the held classes ");
+                   System.out.println("Enter the held classes : ");
                    double h=Double.parseDouble(br.readLine());
-                   System.out.println('Enter the attended classes ");
+                   System.out.println("Enter the attended classes : ");
+                   double at=Double.parseDouble(br.readline());
                    double p=(at/h)*100;
                    System.out.println("percentage is " +p);
-               if(p<75) {
-                 LAttend lat=new LAttend("You are not allowed for java lab exam ");
-                 throw lat; 
-                } 
-           }  
-            catch(LAttend la){
+                   if(p<75) {
+                          LAttend lat=new LAttend("You are not allowed for java lab exam ");
+                        throw lat; 
+                           } 
+                            }  
+                 catch(LAttend la){
                  System.out.println(la);
-           }
-            catch(IOException ie) {
-                 System.out.println(ie);
-           }  
-            finally {
-              if(recordSubmitted){
-               System.out.println("You are not allowed for Lab Exam without record ");
-            } 
-           else{
-                System.out.println("You are allowed for Lab Exam only If u have a Record and attendance ");
                   }
-       }
-   }
- }
+                 catch(IOException ie) {
+                 System.out.println(ie);
+                  }  
+                 finally {
+                    if(recordSubmitted){
+                        System.out.println("YOU ARE NOT ALLOWED FOR LAB EXAM WITHOUT RECORD");
+                       } 
+                    else {
+                      System.out.println("YOU ARE ALLOWED FOR LAB EXAM ONLY IF YOU HAVE A RECORD AND ATTENDANCE ");
+                     }
+                     }
+                     }
+                     }
 
 
  
